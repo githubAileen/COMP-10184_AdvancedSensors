ca29c7bc4c5c69d941ee7f67885ad492a67ea4e1
githubAileen <aileenjlam@gmail.com>
2022-10-03 16:28:54 -0400

    correct files

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,71 @@
 // I Aileen Velasco, 000805966 certify that this material is my original work. 
 //No other person's work has been used without due acknowledgement. 
 //I have not made my work available to anyone else.
-
-// COMP-10184 – Mohawk College 
-// PIR Sensor Test Application 
-// Turn on the blue LED with motion is detected. 
  
+// library for Arduino framework  
 #include <Arduino.h> 
+// 1-Wire sensor communication libary 
+#include <OneWire.h> 
+// DS18B20 sensor library 
+#include <DallasTemperature.h> 
  
-// digital input pin definitions 
-#define PIN_PIR D5 
-#define PIN_BUTTON D6 
+// Pin that the  DS18B20 is connected to 
+const int oneWireBus = D3;      
  
+// Setup a oneWire instance to communicate with any OneWire devices 
+OneWire oneWire(oneWireBus); 
  
-// ************************************************************* 
+// Pass our oneWire reference to Dallas Temperature sensor  
+DallasTemperature DS18B20(&oneWire); 
+
+DeviceAddress deviceAdd;
+ 
 void setup() { 
   // configure the USB serial monitor 
   Serial.begin(115200); 
  
-  // configure the LED output 
-  pinMode(LED_BUILTIN, OUTPUT); 
- 
-  // PIR sensor is an INPUT 
-  pinMode(PIN_PIR, INPUT); 
- 
-  // Button is an INPUT 
-  pinMode(PIN_BUTTON, INPUT_PULLUP); 
+  // Start the DS18B20 sensor 
+  DS18B20.begin(); 
+
+  Serial.println("\nTemperature Application");
+
+  //Check if a device is connected
+  if (!DS18B20.getAddress(deviceAdd, 0)){
+    Serial.println("No DS18B20 temperature sensors are installed");
+  } else {
+    String deviceAddress = "";
+    for (uint8_t i = 0; i < 8; i++)
+    {
+      deviceAddress += String(deviceAdd[i], HEX);
+    }
+
+    Serial.println("Found DS18B20 sensor with address: " + deviceAddress);    
+  }
 } 
  
-// ************************************************************* 
 void loop() { 
-  bool bPIR; 
+  if (DS18B20.getAddress(deviceAdd, 0)){
+  float fTemp; 
+  String judgement = "";
  
-  // read PIR sensor (true = Motion detected!).  As long as there 
-  // is motion, this signal will be true.  About 2.5 seconds after  
-  // motion stops, the PIR signal will become false. 
-  bPIR = digitalRead(PIN_PIR); 
+  // ask DS18B20 for the current temperature 
+  DS18B20.requestTemperatures(); 
  
-  // send the PIR signal directly to the LED 
-  // but invert it because true = LED off! 
-  digitalWrite(LED_BUILTIN, !bPIR); 
+  // fetch the temperature.  We only have 1 sensor, so the index is 0. 
+  fTemp = DS18B20.getTempCByIndex(0); 
+ 
+  // Check temp to store the judgement
+  if(fTemp < 10) judgement = "Cold!";
+  else if (fTemp >= 10 && fTemp < 15) judgement = "Cool";
+  else if (fTemp >= 15 && fTemp < 25) judgement = "Perfect";
+  else if (fTemp >= 25 && fTemp < 30) judgement = "Warm";
+  else if (fTemp >= 30 && fTemp < 35) judgement = "Hot";
+  else judgement = "Too Hot!";
+
+  // print the temp & judgement to the USB serial monitor 
+  Serial.println("Current temperature is: " + String(fTemp) + " C or " + judgement); 
+ 
+  // wait 2s (2000ms) before doing this again 
+  delay(2000); 
+  }
 } 
